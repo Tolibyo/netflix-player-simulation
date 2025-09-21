0eecf6d3da1dc7c6b746e524573d02e79f4fcf89
Tolib Yormahmadov <tolibyormahmadov@Tolibs-MacBook-Air.local>
2025-09-20 21:34:58 -0600

    setup: add folders and tooling (TS, Vitest, ESLint)

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  env: { node: true, es2021: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: { 'no-console': 'off' }
+};
